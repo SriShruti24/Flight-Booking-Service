64850aae1ce54aa795f8bb51acf4c06476fc60ca
Srishruti24 <sri.shruti24@gmail.com>
2026-02-26 01:03:32 +0530

    Added payment logic

--- src/services/booking-service.js
+++ src/services/booking-service.js
@@ -5,8 +5,11 @@ const { BookingRepository } = require("../repositories");
 const { ServerConfig } = require("../config");
 const db = require("../models");
 const AppError = require("../utils/errors/app-error");
+const {Enums}=require('../utils/common');
+const {BOOKED,CANCELLED}=Enums.BOOKING_STATUS;
 
 const bookingRepository=new BookingRepository();
+
 async function createBooking(data) {
   const transaction =await db.sequelize.transaction();
   try{
@@ -32,6 +35,36 @@ async function createBooking(data) {
   }
 }
 
+async function makePayment(data){
+  const transaction=await db.sequelize.transaction();
+  console.log(data);
+  try {
+    const bookingDetails=await bookingRepository.get(data.bookingId,transaction);
+    if(bookingDetails.status==CANCELLED){
+      throw new AppError('The booking has expired',StatusCodes.BAD_REQUEST);
+    }
+    const bookingTime=new Date(bookingDetails.createdAt);
+    const currentTime=new Date();
+    if(currentTime-bookingTime>300000){
+      await bookingRepository.update(data.bookingId,{status:CANCELLED},transaction);
+      throw new AppError('The booking has expired',StatusCodes.BAD_REQUEST);
+    }
+    if(bookingDetails.totalCost!=data.totalCost){
+      throw new AppError("The amount of the payment doesnt match", StatusCodes.BAD_REQUEST);
+    }
+    if(bookingDetails.userId !=data.userId){
+       throw new AppError("The user corresponding to the booking  doesnt match", StatusCodes.BAD_REQUEST);
+    }
+    //we assume here payment is succesfull
+    await bookingRepository.update(data.bookingId,{status:BOOKED},transaction);
+    await transaction.commit();
+  } catch (error) {
+     await transaction.rollback();
+    throw error;
+  }
+}
+
 module.exports = {
   createBooking,
+  makePayment
 };
